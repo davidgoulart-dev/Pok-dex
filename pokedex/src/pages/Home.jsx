c7190163610a03447c47488a9c568f448fa6f5c3
David Goulart <davidgoulart.tae@gmail.com>
2023-04-20 17:46:21 -0300

    cards criados

--- pokedex/src/pages/Home.jsx
+++ pokedex/src/pages/Home.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import PokemonCard from '../components/PokemonCard';
+import { Container } from '@mui/material';
+import styles from './styles.module.scss';
+
 
 
 export const Home = () => {
     return (
-        <><Navbar/>
+        <>
+        <Navbar/>
+               
+        <section className={styles.card}>
         <PokemonCard/>
+        <PokemonCard/>
+        <PokemonCard/>
+        <PokemonCard/>
+        </section>         
         </>
-        
         
     );
 };
