568ede3639b40c349c1cbab427f6f260a509526b
David Goulart <davidgoulart.tae@gmail.com>
2023-04-21 15:22:34 -0300

    Barra de pesquisa adicionada no navbar

--- pokedex/src/components/Navbar/index.jsx
+++ pokedex/src/components/Navbar/index.jsx
@@ -5,11 +5,12 @@ export default function Navbar() {
   return (
     <nav className={styles.navContent}>
         <img src="assets/pokedex.png" alt="" srcset="" />
-        <ul>
-            <li> <a href="/">Home</a> </li> 
-            <li> <a href="/about">About</a> </li>
-            <li> <a href="/contact">Contact</a> </li>
-        </ul>
+       <div className={styles.searchBox}>
+          <input className={styles.searchText} type="text" placeholder="Pesquisar" />
+          <a href='#'>
+            <img className={styles.searchIcon} src="assets/loupe.png" alt="search-icon" srcset="" />
+          </a>
+       </div>
     </nav>
   );
    
